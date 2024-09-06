ee7235d0a78ff3bb5caae0650a32de8d4d8117c8
Akinola Ayobami <ayobamiakinola84@gmail.com>
2024-09-06 10:12:30 +0100

    first commit

---
+++ src/components/SnowBall.tsx
@@ -0,0 +1,13 @@
+import "./SnowBall.css";
+
+export default function SnowBall() {
+	return (
+		<div>
+			<div className="snowball snowball-1"></div>
+			<div className="snowball snowball-2"></div>
+			<div className="snowball snowball-3"></div>
+			<div className="snowball snowball-4"></div>
+			<div className="snowball snowball-5"></div>
+		</div>
+	);
+}
